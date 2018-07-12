7e68fdbeaf87fb239e122d23943f567ba7505aa1
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-07-12 16:58:22 +0200

    implemented send a receive command

---
+++ Spring App/src/main/java/com/tambapps/p2p/peer_transfer/desktop/command/ReceiveCommand.java
@@ -0,0 +1,30 @@
+package com.tambapps.p2p.peer_transfer.desktop.command;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.tambapps.p2p.file_sharing.Peer;
+
+@Parameters(commandDescription = "Receive file from another peer")
+public class ReceiveCommand {
+    @Parameter(names = "-peer", description = "the sending peer", required = true, converter = PeerConverter.class)
+    private Peer peer;
+
+    @Parameter(names = {"-download", "--downloadPath"}, description = "the path where the file(s) will be downloaded",
+            required = true)
+    private String downloadPath;
+
+    @Parameter(names = "-count", description = "the number of files that will be received")
+    private int count = 1;
+
+    public Peer getPeer() {
+        return peer;
+    }
+
+    public String getDownloadPath() {
+        return downloadPath;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
