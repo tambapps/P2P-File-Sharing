78fe02bbd32c1cece54b73d12dd3eb94ba527f66
nelson888 <tambue@hotmail.fr>
2019-11-03 00:02:16 +0100

    created app component

--- desktop_appfx/src/main/java/com/tambapps/p2p/fandem/desktop/App.java
+++ desktop_appfx/src/main/java/com/tambapps/p2p/fandem/desktop/App.java
@@ -4,6 +4,9 @@ import com.tambapps.p2p.fandem.desktop.controller.SendPaneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,15 +18,25 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("sendPane.fxml"));
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("app.fxml"));
+        VBox vBox = loader.load();
+        HBox panesContainer = (HBox) vBox.getChildren().get(0);
+        panesContainer.getChildren().add(loadSendPane(stage));
+        Scene scene = new Scene(vBox);
 
-        Scene scene = new Scene(loader.load());
-        SendPaneController controller = loader.getController();
-        controller.setStage(stage);
+     //   SendPaneController controller = loader.getController();
+       // controller.setStage(stage);
         stage.setScene(scene);
         stage.show();
     }
 
+    private Pane loadSendPane(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("sendPane.fxml"));
+        Pane pane = loader.load();
+        SendPaneController controller = loader.getController();
+        controller.setStage(stage);
+        return pane;
+    }
     public static void main(String[] args) {
         launch();
     }
