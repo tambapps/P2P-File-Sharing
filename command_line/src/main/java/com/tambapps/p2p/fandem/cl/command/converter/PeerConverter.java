a8f8a78ff83679cdd10586a0e9d711bbdda6c588
Nelson Fonkoua <nfonkoua@weborama.com>
2022-11-12 12:41:35 +0100

    throwed parameter exception when validation fails

--- command_line/src/main/java/com/tambapps/p2p/fandem/cl/command/converter/PeerConverter.java
+++ command_line/src/main/java/com/tambapps/p2p/fandem/cl/command/converter/PeerConverter.java
@@ -1,6 +1,7 @@
 package com.tambapps.p2p.fandem.cl.command.converter;
 
 import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.ParameterException;
 import com.tambapps.p2p.fandem.Fandem;
 import com.tambapps.p2p.speer.Peer;
 
@@ -10,10 +11,14 @@ import com.tambapps.p2p.speer.Peer;
 public class PeerConverter implements IStringConverter<Peer> {
   @Override
   public Peer convert(String value) {
-    if (value.contains(".")) {
-      return Peer.parse(value);
-    } else {
-      return Fandem.parsePeerFromHexString(value);
+    try {
+      if (value.contains(".")) {
+        return Peer.parse(value);
+      } else {
+        return Fandem.parsePeerFromHexString(value);
+      }
+    } catch (IllegalArgumentException e) {
+      throw new ParameterException(e.getMessage(), e);
     }
   }
 }
