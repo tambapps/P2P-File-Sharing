fd625a377ae54a7550f93bb7f5b395da678021b6
Nelson Fonkoua <tambue@hotmail.fr>
2023-08-13 23:04:35 +0200

    added pick file button

--- android_app_v2/app/src/main/java/com/tambapps/p2p/peer_transfer/android/SendActivity.kt
+++ android_app_v2/app/src/main/java/com/tambapps/p2p/peer_transfer/android/SendActivity.kt
@@ -3,6 +3,7 @@ package com.tambapps.p2p.peer_transfer.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,8 +11,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,8 +23,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tambapps.p2p.peer_transfer.android.ui.theme.FandemAndroidTheme
 import com.tambapps.p2p.peer_transfer.android.ui.theme.gradientBrush
 import com.tambapps.p2p.peer_transfer.android.ui.theme.mainTextStyle
@@ -47,11 +53,13 @@ fun SendView() {
 
     Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
       Spacer(modifier = Modifier.weight(1f))
-      Text(modifier = Modifier.weight(1f), text = stringResource(id = R.string.pick_a_file_to_send))
+      Text(modifier = Modifier.weight(1f), text = stringResource(id = R.string.pick_a_file_to_send), fontSize = 24.sp)
       Spacer(modifier = Modifier.weight(1f))
-      FloatingActionButton(modifier = Modifier.weight(1f).wrapContentWidth(),
+      FloatingActionButton(modifier = Modifier.size(128.dp),
+        shape = RoundedCornerShape(128.dp),
         onClick = { /*TODO*/ }) {
-
+        Image(painter = painterResource(id = R.drawable.folder), contentDescription = "folder",
+          modifier = Modifier.size(80.dp))
 
       }
       Spacer(modifier = Modifier.weight(1f))
