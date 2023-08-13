984ee9db7958d7c11e8ad050d056de6e882f7276
Nelson Fonkoua <tambue@hotmail.fr>
2023-08-13 23:51:53 +0200

    started handle permission

--- android_app_v2/app/src/main/java/com/tambapps/p2p/peer_transfer/android/SendActivity.kt
+++ android_app_v2/app/src/main/java/com/tambapps/p2p/peer_transfer/android/SendActivity.kt
@@ -1,8 +1,12 @@
 package com.tambapps.p2p.peer_transfer.android
 
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.app.Activity
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -20,30 +24,49 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
 import com.tambapps.p2p.peer_transfer.android.ui.theme.FandemAndroidTheme
 import com.tambapps.p2p.peer_transfer.android.ui.theme.gradientBrush
+import com.tambapps.p2p.peer_transfer.android.util.hasPermission
+
+const val ANY_CONTENT_TYPE = "*/*"
 
 class SendActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    val pickFileLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) {
-      Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show()
-    }
 
     setContent {
       FandemAndroidTheme {
-        SendView(pickFileLauncher)
+        SendView()
       }
     }
   }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun SendView(pickFileLauncher: ActivityResultLauncher<String>) {
+fun SendView() {
+  val context = LocalContext.current as Activity
+  val pickFileLauncher = rememberLauncherForActivityResult(ActivityResultContracts.GetMultipleContents()) {
+    Toast.makeText(context, "TODO", Toast.LENGTH_SHORT).show()
+  }
+
+  // request permission and then pick file
+  val requestPermissionLauncher = rememberLauncherForActivityResult(
+    ActivityResultContracts.RequestPermission()
+  ) { isGranted: Boolean ->
+    if (isGranted) {
+      pickFileLauncher.launch(ANY_CONTENT_TYPE)
+    } else {
+      Toast.makeText(context, R.string.permissions_not_granted, Toast.LENGTH_SHORT).show()
+    }
+  }
   Surface(modifier = Modifier
     .fillMaxSize()
     .background(brush = gradientBrush), color = Color.Transparent) {
@@ -54,7 +77,15 @@ fun SendView(pickFileLauncher: ActivityResultLauncher<String>) {
       Spacer(modifier = Modifier.weight(1f))
       FloatingActionButton(modifier = Modifier.size(128.dp),
         shape = RoundedCornerShape(128.dp),
-        onClick = { pickFileLauncher.launch("*/*") }) {
+        onClick = {
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+            && !context.hasPermission(permission = POST_NOTIFICATIONS)) {
+            // TODO there is a problem with that when permission is denied
+            requestPermissionLauncher.launch(POST_NOTIFICATIONS)
+          } else {
+            pickFileLauncher.launch(ANY_CONTENT_TYPE)
+          }
+        }) {
         Image(painter = painterResource(id = R.drawable.folder), contentDescription = "folder",
           modifier = Modifier.size(80.dp))
       }
---
+++ android_app_v2/app/src/main/java/com/tambapps/p2p/peer_transfer/android/util/ContextExtensions.kt
@@ -0,0 +1,8 @@
+package com.tambapps.p2p.peer_transfer.android.util
+
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+
+fun Context.hasPermission(permission: String) = ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
